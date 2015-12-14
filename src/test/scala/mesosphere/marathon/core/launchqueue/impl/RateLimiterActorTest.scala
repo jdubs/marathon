f51adb7ff7e10e14c2fc9b92b07269c2b173c421
Peter Kolloch <peter@mesosphere.io>
2015-12-14 18:43:20 +0100

    Toward #462 - create interface traits for TaskTracker
    ..to ease later replacement of the implementation.
    
    Also move `determineOverdueTasks` to `KillOverdueTasksActor` to
    shrink the TaskTracker interface since this method is only called
    there.

--- src/test/scala/mesosphere/marathon/core/launchqueue/impl/RateLimiterActorTest.scala
+++ src/test/scala/mesosphere/marathon/core/launchqueue/impl/RateLimiterActorTest.scala
@@ -9,7 +9,7 @@ import akka.util.Timeout
 import mesosphere.marathon.MarathonSpec
 import mesosphere.marathon.core.base.ConstantClock
 import mesosphere.marathon.state.{ AppDefinition, AppRepository, PathId }
-import mesosphere.marathon.tasks.TaskTracker
+import mesosphere.marathon.tasks.{ TaskTracker, TaskTrackerImpl }
 import org.mockito.Mockito
 
 import scala.concurrent.Await
@@ -65,7 +65,7 @@ class RateLimiterActorTest extends MarathonSpec {
     appRepository = mock[AppRepository]
     updateReceiver = TestProbe()
     val props = RateLimiterActor.props(
-      rateLimiter, taskTracker, appRepository, updateReceiver.ref)
+      rateLimiter, appRepository, updateReceiver.ref)
     limiterRef = actorSystem.actorOf(props, "limiter")
   }
 
